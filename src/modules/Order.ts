ec8fa8ce268225b840cff30676269f28e6be75c3
Luis Schliesske <36712489+gitpusha@users.noreply.github.com>
2021-04-10 11:09:29 +0200

    Update src/modules/Order.ts

--- src/modules/Order.ts
+++ src/modules/Order.ts
@@ -8,7 +8,7 @@ export function getAddressByNetwork(network: string): Address {
   if (network == 'mainnet') {
     return Address.fromString('0x36049D479A97CdE1fC6E2a5D2caE30B666Ebf92B')
   } if (network == 'ropsten') {
-    return Address.fromString('0xb6c2E1B5AB82d8e555ABAa00BAc4606Ab5EA4668')
+    return Address.fromString('0x0e5096D201Fe2985f5C26432A76f145D6e5D1453')
   }
 
   throw 'No Address specified'
