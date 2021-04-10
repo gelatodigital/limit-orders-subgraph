12185bdf5b16291e20608fb2b5c4a320c04f7b00
Luis Schliesske <36712489+gitpusha@users.noreply.github.com>
2021-04-10 11:56:58 +0200

    chore: update deps and redeploy limit-orders-ropsten (#2)

--- src/modules/Order.ts
+++ src/modules/Order.ts
@@ -1,15 +1,15 @@
-import { Address } from "@graphprotocol/graph-ts"
+import { Address } from "@graphprotocol/graph-ts";
 
-export const OPEN = 'open'
-export const EXECUTED = 'executed'
-export const CANCELLED = 'cancelled'
+export const OPEN = "open";
+export const EXECUTED = "executed";
+export const CANCELLED = "cancelled";
 
 export function getAddressByNetwork(network: string): Address {
-  if (network == 'mainnet') {
-    return Address.fromString('0x36049D479A97CdE1fC6E2a5D2caE30B666Ebf92B')
-  } if (network == 'ropsten') {
-    return Address.fromString('0x0e5096D201Fe2985f5C26432A76f145D6e5D1453')
-  }
+  if (network == "mainnet")
+    return Address.fromString("0x36049D479A97CdE1fC6E2a5D2caE30B666Ebf92B");
 
-  throw 'No Address specified'
+  if (network == "ropsten")
+    return Address.fromString("0x0e5096D201Fe2985f5C26432A76f145D6e5D1453");
+
+  throw "No Address specified";
 }
