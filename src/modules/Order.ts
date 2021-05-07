946ed9193a1004d3dae000aa9576f48992ec41a8
gitpusha <gitpusha@gmail.com>
2021-05-07 22:54:49 +0200

    config: deploy limit-orders-polygon

--- src/modules/Order.ts
+++ src/modules/Order.ts
@@ -8,6 +8,9 @@ export function getAddressByNetwork(network: string): Address {
   if (network == "mainnet")
     return Address.fromString("0x36049D479A97CdE1fC6E2a5D2caE30B666Ebf92B");
 
+  if (network == "matic")
+    return Address.fromString("0x38c4092b28dAB7F3d98eE6524549571c283cdfA5");
+
   if (network == "ropsten")
     return Address.fromString("0x0e5096D201Fe2985f5C26432A76f145D6e5D1453");
 
